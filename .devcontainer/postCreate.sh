9669044e429121ffcc00a64ae1b6975bb5b7e331
Himanshu Singh <himanshu@arista.com>
2025-05-22 19:03:43 +0530

    gRPC demo

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -5,7 +5,4 @@ set +e
 # Install required packages
 bash -c "$(curl -sL https://get-gnmic.openconfig.net)"
 python3 -m pip install --upgrade pip
-python3 -m pip install pyang
-python3 -m pip install pyangbind
-python3 -m pip install ncclient
-python3 -m pip install requests
+python3 -m pip install -r .devcontainer/requirements.txt
