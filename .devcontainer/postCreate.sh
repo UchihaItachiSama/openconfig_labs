3a90d9190e94fbf4c700924e1907fd0e71b1c9e1
Himanshu Singh <himanshu@arista.com>
2025-07-15 17:17:49 +0530

    update-lab-files

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -4,6 +4,6 @@ set +e
 
 # Install required packages
 bash -c "$(curl -sL https://get-gnmic.openconfig.net)"
-# go install github.com/aristanetworks/goarista/cmd/gnmi@latest
+bash -c "$(curl -sL https://get-gnoic.kmrd.dev)"
 python3 -m pip install --upgrade pip
 python3 -m pip install -r .devcontainer/requirements.txt
