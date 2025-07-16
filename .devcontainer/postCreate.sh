105d896b525b0d51c8a009e514f5aa1998d2c596
Himanshu Singh <himanshu@arista.com>
2025-07-16 12:46:14 +0530

    Update labs (#3)
    * update-lab-files
    
    * add dood
    
    * add dood file
    
    * add alpine client
    
    * add links
    
    * update readme

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,58 @@
 #!/bin/bash
 
+pull_arch_specific_ceos_image() {
+    #Get the architecture
+    local arch=$(uname -m)
+    
+    # Based on arch pull arm or 64bit image
+    case ${arch} in
+        x86_64)
+            echo "Detected AMD64/x86_64 architecture. Pulling cEOS64-lab image"
+            ardl get eos --format cEOS64 --version 4.34.1F --import-docker
+            return $?
+            ;;
+        aarch64|arm64)
+            echo "Detected ARM64/aarch64 architecture. Pulling cEOSarm-lab image"
+            ardl get eos --format cEOSarm --version 4.34.1F --import-docker
+            return $?
+            ;;
+        *)
+            echo "Error: Unknown architecture: ${arch}"
+            return 1
+            ;;
+    esac
+}
+
+install_images() {
+    #Pull cEOS-Lab image
+    pull_arch_specific_ceos_image
+    
+    #Pull srlinux image
+    docker pull ghcr.io/nokia/srlinux:25.3.2
+    
+    #Build client image
+    (
+        cd alpine_host || exit 1
+        docker build -t alpine-host .
+    )
+    
+    #Pull gnmic image
+    docker pull ghcr.io/openconfig/gnmic:latest
+}
+
+install_packages() {
+    #Install gnmic
+    bash -c "$(curl -sL https://get-gnmic.openconfig.net)"
+    
+    #Install gnoic
+    #bash -c "$(curl -sL https://get-gnoic.kmrd.dev)"
+    
+    #Upgrade pip and install requirements
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r .devcontainer/requirements.txt
+}
+
 set +e
 
-# Install required packages
-bash -c "$(curl -sL https://get-gnmic.openconfig.net)"
-# go install github.com/aristanetworks/goarista/cmd/gnmi@latest
-python3 -m pip install --upgrade pip
-python3 -m pip install -r .devcontainer/requirements.txt
+install_packages
+install_images
--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # OpenConfig Labs
 
+[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/UchihaItachiSama/openconfig_labs?quickstart=1&devcontainer_path=.devcontainer%2Fdocker-in-docker%2Fdevcontainer.json)
+[![Open in DevPod!](https://devpod.sh/assets/open-in-devpod.svg)](https://devpod.sh/open#https://github.com/UchihaItachiSama/openconfig_labs)
+
+*For more details on deploying labs refer to [lab documentation](/labs/README.md)*
+
 Welcome to the OpenConfig training lab! This repository contains lab resources and demos that accompany the OpenConfig training course.
 
-*For access to the slides please reach out to the maintainer. The labs and demo material in this repository can be used independently as well for any of the individual topics.*
+*For access to the slides please reach out to the maintainer.*
+
+The labs and demo material in this repository can be used independently as well for any of the individual topics.
 
 Refer to the `README.md` under each folder for more details.
 
@@ -15,4 +22,4 @@ Refer to the `README.md` under each folder for more details.
 - gRPC
 - gNMI
   - gnmic
-  - gNMI gateway
+- gNMI Dial-out via gRPC Tunnel
